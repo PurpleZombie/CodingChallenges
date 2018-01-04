1e707f41decc149c84e340b7194b6d858f977d05
PurpleZombie <vishaltvijay@gmail.com>
2018-01-04 12:13:47 -0600

    Update Test

--- Algorithms/src/ReadInput.java
+++ Algorithms/src/ReadInput.java
@@ -1,8 +1,15 @@
+import java.util.Scanner;
 
 public class ReadInput {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter array");
+		String[] inps=sc.nextLine().split(" ");
+		int[] inp=new int[inps.length];
+		for(int i=0;i<inp.length;i++) 
+			inp[i]=Integer.parseInt(inps[i]);
 
 	}
 
