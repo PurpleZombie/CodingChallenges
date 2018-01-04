1381ce156f7b86ba44f70fd1b9a9283eab2b6f30
PurpleZombie <vishaltvijay@gmail.com>
2018-01-04 12:11:10 -0600

    Setup for practice

---
+++ Algorithms/src/MergeSort.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class MergeSort {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter array");
+		String[] inps=sc.nextLine().split(" ");
+		int[] inp=new int[inps.length];
+		for(int i=0;i<inp.length;i++) 
+			inp[i]=Integer.parseInt(inps[i]);
+		
+	}
+
+}
